450d044ed2d7ea3215f9c750c8fa9f4a42ada407
Quentin Briançon <quentin.briancon@gmail.com>
2023-11-25 16:44:18 +0100

    Parser Lobby

--- Among Legend V2/Parser_Team.py
+++ Among Legend V2/Parser_Team.py
@@ -8,7 +8,7 @@ from Player import Player
 
 teams={}
 
-
+# Crée l'équipe
 async def CreateTeam(ctx):
     content = ctx.message.content
     command = ctx.command.name  # Nom de la commande utilisée (dans ce cas, "CreateTeam")
@@ -32,6 +32,7 @@ async def CreateTeam(ctx):
     await teams[team_name].create_team(ctx, team_name)
 
 
+# Modifie l'équipe
 async def ModifyTeam(ctx, team_name, index1: int, index2: int):
     if team_name not in teams:
         await ctx.send(f"L'equipe {team_name} n'existe pas. Creez une equipe en utilisant !CreateTeam.")
@@ -39,6 +40,7 @@ async def ModifyTeam(ctx, team_name, index1: int, index2: int):
     await teams[team_name].modify(ctx, index1, index2)
 
 
+# Affiche les équipes
 async def ShowTeams(ctx):
     if teams == {}:
         await ctx.send(f"Aucune equipe n'a ete creee pour le moment. Creez une equipe en utilisant !CreateTeam.")
@@ -47,6 +49,7 @@ async def ShowTeams(ctx):
         await teams[team_name].show_team(ctx)
 
 
+# Supprime l'équipe
 async def DeleteTeam(ctx, team_name):
     if team_name not in teams:
         await ctx.send(f"L'equipe {team_name} n'existe pas.")
@@ -54,6 +57,7 @@ async def DeleteTeam(ctx, team_name):
     del teams[team_name]
     await ctx.send(f"L'equipe {team_name} a ete supprimee.")
 
+#Echange deux joueurs entre deux equipes
 async def SwitchPlayer(ctx, team_name1, index1: int, team_name2, index2: int):
     if team_name1 not in teams or team_name2 not in teams:
         if team_name1 not in teams:
